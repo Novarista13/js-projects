86f014f41306e0738ae0c30ae0484a9ae8e2f19b
Novarista13 <134473935+Novarista13@users.noreply.github.com>
2024-05-05 17:34:49 +0630

    added pjs

---
+++ Example/js/app.js
@@ -0,0 +1,5 @@
+import { PI, add } from "./math";
+
+console.log(PI);
+console.log(add(1, 2));
+
---
+++ Example/js/math.js
@@ -0,0 +1,21 @@
+// const PI = 3.14;
+
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// module.exports = { PI, add };
+
+export const PI = 3.14;
+
+export function sum(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
